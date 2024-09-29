239173ca686a499b690acfcb021cf4c2ac9478cf
yatin <yatin.aggarwal033@gmail.com>
2024-09-29 18:34:21 +0530

    updates commit

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -3,7 +3,7 @@ import Project_Discription from "./Project_Discription.js";
 function Card(props){
     return(
         <>
-            <div className={"  text-amber-50  bg-[#0B1224] w-[50%] h-full"}>
+            <div className={"  text-amber-50  bg-[#0B1224] w-[60%] h-full"}>
                 <div
                     className="  ml-[10%] w-[80%] h-[10%] flex items-center pl-[2%] rounded-t border-2 border-b-[#181A4B]">
                     <button disabled className="bg-rose-400 mr-[2%] rounded-full h-3 w-3"></button>
@@ -12,11 +12,11 @@ function Card(props){
                     <div className="pl-[18%] text-[#54eb13] font-extrabold text-2xl ">{props.title}</div>
                 </div>
                 <div
-                    className=" font-medium  font-mono ml-[10%] w-[80%]  h-[80%]   pl-[2%]  border-2 border-t-[#181A4B]  ">
-                    <div className="flex text-md h-[50%] pt-[3%]">
+                    className=" font-medium flex flex-col  font-mono ml-[10%] w-[80%]  h-[70%]   pl-[2%]  border-2 border-t-[#181A4B]  ">
+                    <div className="flex  h-[85%] pt-[3%]">
                         {props.content}
                     </div>
-                    <div className="flex pl-[2%] h-[45%]  items-end  ">
+                    <div className="flex pl-[2%]   items-end  ">
                         <a className=" text-xl" href={props.github} ><button className="bg-purple-600 p-[15%] rounded-xl font-bold ">Github</button></a>
                     </div>
 
